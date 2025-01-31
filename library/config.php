c68885cbbd01b8ff9fc6744bba7122ba5ab92f4e
Aulia Fatmawati <aulia.fatmawati196@smk.belajar.id>
2025-01-31 09:23:29 +0700

    Aplikasi Dompet Santri
    -Landing
    -Login
    -Logout

---
+++ library/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "domsan";
+
+$con = mysqli_connect($host, $user, $pass, $db);
+
+if(mysqli_connect_error()) {
+    echo "Koneksi gagal ! : " .mysqli_connect_error();
+}
+?>
